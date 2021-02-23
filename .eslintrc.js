77aa97ad7b0a6fe8de1a943722ec4d5ff5cb2029
SunnyLLXX <1632846502@qq.com>
2021-02-23 15:14:29 +0800

    data echarts

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,7 +13,10 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'space-before-function-paren': 0
+    'generator-star-spacing': 'off',
+    'no-tabs':'off',
+    'no-unused-vars':'off',
+    'no-irregular-whitespace':'off'
   },
   parserOptions: {
     parser: 'babel-eslint'
--- src/components/Report.vue
+++ src/components/Report.vue
@@ -7,26 +7,48 @@
             <el-breadcrumb-item>数据报表</el-breadcrumb-item>
         </el-breadcrumb>
         <!-- 卡片区域 -->
-        <el-card></el-card>
+        <el-card>
+           <div id="main" style="width: 600px;height:400px;"></div>
+        </el-card>
     </div>
 </template>
 
 <script>
-// 折线
-// import 'echarts/lib/chart/line'
-// 饼状图
-// import 'echarts/lib/chart/pie'
-// 柱状图
-// import 'echarts/lib/chart/bar'
-// 提示
-// import 'echarts/lib/component/tooltip'
-// 图例
-// import 'echarts/lib/component/legend'
-// 标题
-// import 'echarts/lib/component/title'
+import echarts from 'echarts'
 export default {
   data () {
     return {}
+  },
+  async mounted(){
+    //mouted表示页面元素已被渲染完毕
+    // 基于准备好的dom，初始化echarts实例
+    var myChart = echarts.init(document.getElementById('main'));
+    const res = await this.$http.get('/report/type/1')
+    if(res.data.meta.status != 200){
+      return this.$message.error("获取数据失败")
+    }
+     // 指定图表的配置项和数据
+        // var option = {
+        //     title: {
+        //         text: 'ECharts 入门示例'
+        //     },
+        //     tooltip: {},
+        //     legend: {
+        //         data:['销量']
+        //     },
+        //     xAxis: {
+        //         data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
+        //     },
+        //     yAxis: {},
+        //     series: [{
+        //         name: '销量',
+        //         type: 'bar',
+        //         data: [5, 20, 36, 10, 10, 20]
+        //     }]
+        // };
+
+        // 使用刚指定的配置项和数据显示图表。
+        myChart.setOption(res.data);
   }
 }
 </script>
