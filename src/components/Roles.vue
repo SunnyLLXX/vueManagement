16b78c47975b0dc98e8add0e0d547dc597f55f2f
SunnyLLXX <1632846502@qq.com>
2021-03-02 10:13:38 +0800

    modify details

--- src/components/Roles.vue
+++ src/components/Roles.vue
@@ -96,7 +96,7 @@ export default {
       this.roleslist = res.data.data
     },
     removeRightsById: async function (role, rightid) {
-      const resconfirm = await this.$confirm('此操作将永久删除该文件, 是否继续?', '提示', {
+      const resconfirm = await this.$confirm('此操作将永久删除该权限, 是否继续?', '提示', {
         confirmButtonText: '确定',
         cancelButtonText: '取消',
         type: 'warning'
